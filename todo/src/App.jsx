e3aa2876a6ac5cf01c6da2148e19986cc15075f8
Sharafath_V <142240872+sharafath07@users.noreply.github.com>
2025-08-26 10:29:21 +0000

    added status system

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     <div className="app">
       <div className="mainHeading">
         <h1>ToDo List</h1>
+        <hr size="5px" width="110px" />
+
       </div>
       <div className="subHeading">
         <br />
@@ -18,22 +20,40 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
+        <i onClick={()=>setToDos([...toDos,{id: Date.now(), Text: toDo, status: false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        { toDos.map((value)=>{
+        { toDos.map((obj)=>{
 
         return (
           <div className="todo">
             <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{value}</p>
+              <input onChange={(e)=>{
+                console.log(obj);
+                console.log(e.target.checked);
+                setToDos(toDos.filter(obj2=>{
+                  if(obj2.id === obj.id)
+                    obj2.status = e.target.checked
+                  return obj2 
+                }))
+                
+              }} value={obj.status} type="checkbox" name="" id={obj.id} />
+              <p>{obj.Text}</p>
             </div>
             <div className="right">
               <i className="fas fa-times"></i>
             </div>
           </div>
         )  
+        })}
+
+        <h2>Active Task</h2>
+        <hr size="5px" width="100px" />
+        { toDos.map((obj)=>{
+          if(obj.status){
+            return ( <h3>{obj.Text}</h3> )  
+          }
+          return null
         })}
       </div>
     </div>
